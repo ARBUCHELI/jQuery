295629a3ec123f6da57451078addfc7e0162b367
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-12-24 00:49:37 +0300

    Create app.js

---
+++ Lesson 2: The tricks: DOM Manipulation/Toggling Classes Quiz/app.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, remove the class 'featured' from Article #2 and add it to Article #3!
+
+You must use jQuery's toggleClass method!
+*/
+
+// don't change these variable!
+var article2, article3;
+
+article2 = $('.featured').toggleClass('featured');
+article3 = article2.next().toggleClass('featured');
