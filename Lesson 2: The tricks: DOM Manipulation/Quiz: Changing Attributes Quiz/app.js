0f9ffa23033965ed983ce8e100142f649b8c6001
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2020-12-24 01:25:00 +0300

    Create app.js

---
+++ Lesson 2: The tricks: DOM Manipulation/Quiz: Changing Attributes Quiz/app.js
@@ -0,0 +1,14 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+var navList, firstItem, link;
+
+navList = $('.nav-list');
+firstItem = $('.nav-list').children().first();
+link = firstItem.find('a');
+
+link.attr("href","#1");
